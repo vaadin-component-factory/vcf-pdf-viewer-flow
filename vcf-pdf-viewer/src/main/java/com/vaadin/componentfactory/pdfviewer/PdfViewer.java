96aab59f9d4db46fd39e9654e2cbc58b1152d95d
Paola De Bartolo <paola.debartolo@gmail.com>
2021-11-16 12:17:01 -0300

    feat: add download button

--- vcf-pdf-viewer/src/main/java/com/vaadin/componentfactory/pdfviewer/PdfViewer.java
+++ vcf-pdf-viewer/src/main/java/com/vaadin/componentfactory/pdfviewer/PdfViewer.java
@@ -21,20 +21,27 @@
 package com.vaadin.componentfactory.pdfviewer;
 
 import com.vaadin.componentfactory.pdfviewer.event.ThumbnailClickedEvent;
+import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.Synchronize;
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.server.AbstractStreamResource;
 
 @Tag("vcf-pdf-viewer")
 @NpmPackage(value = "@vaadin-component-factory/vcf-pdf-viewer", version = "^0.9.1")
 @JsModule("@vaadin-component-factory/vcf-pdf-viewer/vcf-pdf-viewer.js")
+@CssImport(value = "./styles/download-button.css", themeFor = "vaadin-button")
 public class PdfViewer extends Div {
 
-  public PdfViewer() {}  
+  public PdfViewer() {}
 
   /**
    * @return url to source file
@@ -53,7 +60,7 @@ public class PdfViewer extends Div {
   }
 
   /**
-   * Sets a pdf file to render as a StreamResource. 
+   * Sets a pdf file to render as a StreamResource.
    * <p>
    * Example: {@code StreamResource resource = new StreamResource("mypdf.pdf", ()
    * -&gt; getPdfInputStream("mypdf.pdf");}
@@ -72,19 +79,17 @@ public class PdfViewer extends Div {
   }
 
   /**
-   * Sets the level of zoom on the rendered pdf file. 
+   * Sets the level of zoom on the rendered pdf file.
    * <p>
-   * Allowed values are 
-   * - Number: for zoom percentage (e.g. 1.5 means 150% zoom). 
-   * - 'auto': default value.
-   * - 'page-fit': fit a full page into component.
+   * Allowed values are - Number: for zoom percentage (e.g. 1.5 means 150% zoom). - 'auto': default
+   * value. - 'page-fit': fit a full page into component.
    * 
    * @param zoom zoom level
    */
   public void setZoom(String zoom) {
     getElement().setProperty("zoom", zoom);
   }
-  
+
   /**
    * Adds a listener for {@link ThumbnailClickedEvent} to the component.
    *
@@ -93,23 +98,23 @@ public class PdfViewer extends Div {
   public void addThumbnailClickedListener(ComponentEventListener<ThumbnailClickedEvent> listener) {
     addListener(ThumbnailClickedEvent.class, listener);
   }
-   
+
   /**
-   * Opens thumbnails view. 
+   * Opens thumbnails view.
    */
   public void openThumbnailsView() {
     getElement().executeJs("this.__openSidebar()");
   }
-  
+
   /**
    * Closes thumbnails view.
    */
   public void closeThumbnailsView() {
     getElement().executeJs("this.__closeSidebar()");
   }
-  
+
   /**
-   * @return current page on viewer 
+   * @return current page on viewer
    */
   @Synchronize("currentPage-changed")
   public String getCurrentPage() {
@@ -124,5 +129,24 @@ public class PdfViewer extends Div {
   public void setPage(Integer currentPage) {
     getElement().executeJs("this.setCurrentPage($0)", currentPage);
   }
-  
+
+  @Override
+  protected void onAttach(AttachEvent attachEvent) {
+    super.onAttach(attachEvent);
+    addDownloadButton();
+  }
+
+  /**
+   * Adds button to download pdf file currently on display.
+   */
+  private void addDownloadButton() {
+    String src = this.getSrc(); 
+    Anchor link = new Anchor(src, "");   
+    Button downloadButton = new Button();
+    downloadButton.getElement().appendChild(new Icon(VaadinIcon.DOWNLOAD).getElement());
+    downloadButton.setThemeName("download-button");
+    link.add(downloadButton);
+    link.getElement().setAttribute("download", true);
+    getElement().appendChild(link.getElement());
+  }
 }
