ae69b305ce9d4a709132b58805b1cc9790799d4d
Paola De Bartolo <paola.debartolo@gmail.com>
2026-01-12 17:24:45 -0300

    build(demo): add AppShellConfiguratorImpl class

---
+++ vcf-pdf-viewer-demo/src/main/java/com/vaadin/componentfactory/pdfviewer/AppShellConfiguratorImpl.java
@@ -0,0 +1,10 @@
+package com.vaadin.componentfactory.pdfviewer;
+
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.lumo.Lumo;
+
+@StyleSheet(Lumo.STYLESHEET)
+public class AppShellConfiguratorImpl implements AppShellConfigurator {
+
+}
