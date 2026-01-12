a8b6be11c7ba1b694b21a0628210bcca989ff62b
Paola De Bartolo <paola.debartolo@gmail.com>
2026-01-12 17:37:05 -0300

    chore: update license headers

--- vcf-pdf-viewer/src/main/resources/META-INF/resources/frontend/print-js/dist/print.js
+++ vcf-pdf-viewer/src/main/resources/META-INF/resources/frontend/print-js/dist/print.js
@@ -2,7 +2,7 @@
  * #%L
  * Pdf Viewer
  * %%
- * Copyright (C) 2022 Vaadin Ltd
+ * Copyright (C) 2022 - 2026 Vaadin Ltd
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * #L%
+ */
+
+/**
+ *  The MIT License (MIT)
+ * 
+ *  Copyright (c) 2016-present, Rodrigo Vieira (@crabbly)
  */
 
 (function webpackUniversalModuleDefinition(root, factory) {
